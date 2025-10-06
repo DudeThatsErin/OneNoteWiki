8d31f7bb0b740ee829d0e56f91e45fefc5a96d34
Erin <dudethatserin@outlook.com>
2025-10-05 19:54:12 -0500

    Builds successfully locally

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   trailingSlash: true,
-  distDir: 'out',
-  images: {
-    unoptimized: true
-  }
 }
 
 module.exports = nextConfig
